826e7dea668d1bac24fe3df4db97a4b485973edc
Yong Wen Chua <lawliet89@users.noreply.github.com>
2015-07-21 17:10:42 +0100

    First code push

---
+++ FeedDotNetBot/Bot.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FeedDotNetBot.Properties;
+using Nito.AsyncEx;
+using Telegram.Bot;
+
+namespace FeedDotNetBot
+{
+    public class Bot : IDisposableRunnable
+    {
+        private readonly AsyncContextThread asyncContextThread;
+        private readonly CancellationTokenSource cancellationTokenSource;
+        private readonly Api api;
+        private int updateOffset = 0;
+
+        public Bot()
+        {
+            api = new Api(Settings.Default.TelegramApiKey);
+            cancellationTokenSource = new CancellationTokenSource();
+            asyncContextThread = new AsyncContextThread();
+        }
+
+        public Task Run()
+        {
+            return asyncContextThread.Factory.Run(async () =>
+            {
+                // First let's check that we actually have the right tokens
+                try
+                {
+                    var user = await GetNameAsync();
+                    Console.WriteLine("I am {0}!", user);
+                    await scheduleUpdatesAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Program.Terminate(1, false);
+                }
+            });
+        }
+
+        public async Task<string> GetNameAsync()
+        {
+            var user = await api.GetMe();
+            return user.Username;
+        }
+
+        private async Task scheduleUpdatesAsync()
+        {
+            var interval = Settings.Default.BotUpdateInterval;
+            while (true)
+            {
+                if (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    await getUpdatesAsync();
+                    await Task.Delay(interval);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private async Task getUpdatesAsync()
+        {
+            var lastUpdateOffset = updateOffset;
+            var updates = await api.GetUpdates(lastUpdateOffset);
+            
+            foreach (var update in updates)
+            {
+                lastUpdateOffset = update.Id;
+                Console.WriteLine(update.Message.Text);
+            }
+            updateOffset = lastUpdateOffset + 1;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            asyncContextThread.Dispose();
+        }
+    }
+}
---
+++ FeedDotNetBot/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FeedDotNetBot.Properties;
+using Nito.AsyncEx;
+
+namespace FeedDotNetBot
+{
+    class Program
+    {
+        private static TaskCompletionSource<int> mainTaskCompletionSource;
+
+        // Entry point to the bot
+        static int Main(string[] args)
+        {
+            return AsyncContext.Run(AsyncMain);
+        }
+
+        static async Task<int> AsyncMain()
+        {
+            mainTaskCompletionSource = new TaskCompletionSource<int>();
+            Console.CancelKeyPress += (s, e) => Terminate();
+
+            if (string.IsNullOrEmpty(Settings.Default.TelegramApiKey))
+            {
+                Console.WriteLine(
+                    "You need to open 'FeedDotNetBot.exe.config.xml' and put your Telegram API key in there");
+                Terminate(1, false);
+                return await mainTaskCompletionSource.Task;
+            }
+            else
+            {
+                Console.WriteLine("API Key: {0}", Settings.Default.TelegramApiKey);
+                Console.WriteLine("Press CTRL + C to exit");
+                return await run();
+            }
+        }
+
+        private static async Task<int> run()
+        {
+            var runnables = new IDisposableRunnable[] {new Bot()};
+            try
+            {
+                var aggregateTasks = new List<Task>();
+                aggregateTasks.AddRange(runnables.Select(runnable => runnable.Run()));
+                aggregateTasks.Add(mainTaskCompletionSource.Task);
+                await Task.WhenAny(aggregateTasks);
+                return mainTaskCompletionSource.Task.IsCompleted ? mainTaskCompletionSource.Task.Result : 1;
+            }
+            finally
+            {
+                foreach (var runnable in runnables)
+                    runnable.Dispose();
+            }
+        }
+
+        public static void Terminate(int code = 0, bool terminateImmediately = true)
+        {
+            if (!terminateImmediately)
+            {
+                Console.WriteLine("Press enter to exit");
+                Console.Read();
+            }
+            mainTaskCompletionSource.SetResult(code);
+        }
+    }
+}
